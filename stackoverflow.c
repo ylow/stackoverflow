6459b9af2858cb7cbc0f37fe8971f65c4d9204bc
Yucheng Low <2740522+ylow@users.noreply.github.com>
2023-02-16 16:50:40 -0800

    ok. something smashable

--- stackoverflow.c
+++ stackoverflow.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+char* user1 = "pika";
+char* password1 = "pwd";
+
+char username[8] = {0};
+char password[8] = {0};
+int userpasswordmatch = 0;
+int admin = 0;
+
 int main() {
-  char c[16] = {0};
-  gets(c);
-  printf("You entered %s\n", c);
+
+  printf("Username: ");
+  gets(username);
+
+  // if username is admin it is always ok
+  admin = (strcmp(username, "admin") == 0);
+
+  printf("Password: ");
+  gets(password);
+
+  userpasswordmatch = strcmp(username, user1) == 0 && 
+                      strcmp(password, password1) == 0;
+
+  if (admin || userpasswordmatch) {
+    printf("Pass!\n");
+  } else {
+    printf("Pass!\n");
+  }
 }
